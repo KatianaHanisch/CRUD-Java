68b5e067edb3afcd1e3e7c8cadfc4b47e0d8a2c5
KatianaHanisch <katianahanisch@gmail.com>
2023-10-21 16:45:22 -0400

    mudancas na classe terceirizado

--- src/main/java/com/apicomsqlite/poo/enity/Terceirizado.java
+++ src/main/java/com/apicomsqlite/poo/enity/Terceirizado.java
@@ -1,11 +1,16 @@
 package com.apicomsqlite.poo.enity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 
 public class Terceirizado {
 
+    @Id
+    private int id;
+
+    private String nome;
     private String funcao;
 
     public String getFuncao() {
@@ -14,6 +19,22 @@ public class Terceirizado {
 
     public void setFuncao(String funcao) {
         this.funcao = funcao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
 }
